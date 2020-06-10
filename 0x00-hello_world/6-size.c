d01960b1e9d1353bc9a531b2452006965689308e
dlscoccia <2054@gmail.com>
2020-06-10 23:38:11 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,19 +9,19 @@ float floatType;
 long int longintType;
 long long int longlongType;
 
-printf("Size of a char: %ld\n",
+printf("Size of a char: %ld byte(s)\n",
 sizeof(charType));
 
-printf("Size of an int: %ld\n",
+printf("Size of an int: %ld byte(s)\n",
 sizeof(integerType));
 
-printf("Size of a long int: %ld\n",
+printf("Size of a long int: %ld byte(s)\n",
 sizeof(longintType));
 
-printf("Size of a long long int: %ld\n",
+printf("Size of a long long int: %ld byte(s)\n",
 sizeof(longlongType));
 
-printf("Size of a float: %ld\n",
+printf("Size of a float: %ld byte(s)\n",
 sizeof(floatType));
 
 return (0);
