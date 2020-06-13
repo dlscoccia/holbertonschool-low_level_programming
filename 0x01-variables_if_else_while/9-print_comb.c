5e93f531398a8c8b10181758e4a54be80375f37a
dlscoccia <2054@gmail.com>
2020-06-13 03:04:21 +0000

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
 /*
- * main
- */int main(void)
+* main
+*/int main(void)
 {
 int c;
 c = '0';
-while (c < '9')
+while (c <= '9')
 {
 putchar(c);
 c = c + 1;
+if (c <= '9')
+{
 putchar(',');
 putchar(' ');
 }
-putchar('9');
+}
+putchar('\n');
 return (0);
 }
