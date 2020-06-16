8113cc72ff9f0546fd19fefeefd8704b3862b2f1
dlscoccia <2054@gmail.com>
2020-06-16 20:32:12 +0000

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
-
-int _islower(int c)
+/*
+ * check for lowercase
+ */int _islower(int c)
 {
-
-  if (c >= 'a' && c <= 'z') 
-    return (1);
-  else 
-    return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
