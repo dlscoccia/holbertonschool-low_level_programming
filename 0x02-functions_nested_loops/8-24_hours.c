2ff749ab339e4518fac98be165f778ef684634ec
dlscoccia <2054@gmail.com>
2020-06-17 18:46:46 +0000

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+void jack_bauer(void)
+{
+  int h;
+  int m;
+  int d;
+
+  h = 0;
+  while (h < 24)
+    {
+      m = 0;
+      while ( m < 60)
+	{ 
+	  d = h / 10;
+	  _putchar('0' + d);
+	  d = h % 10;
+	  _putchar('0' + d);
+	  _putchar(':');
+	  d = m / 10;
+	  _putchar('0' + d);
+	  d = m % 10;
+	  _putchar('0' + d);
+	  _putchar('\n');
+	  m++;
+	  }
+      h++;
+	}
+}
