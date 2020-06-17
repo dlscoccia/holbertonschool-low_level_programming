f8b40a69d4b148566b9509b7b5ff20e0f62085d0
dlscoccia <2054@gmail.com>
2020-06-17 02:19:08 +0000

    ig

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,12 +1,10 @@
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
 #include <stdio.h>
-
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
 #endif /* HOLBERTON_H */
-
