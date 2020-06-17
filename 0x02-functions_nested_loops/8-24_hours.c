ee017b77246f5c7cf2adaa3594c70d2a6f332129
dlscoccia <2054@gmail.com>
2020-06-17 18:53:37 +0000

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,28 +2,27 @@
 
 void jack_bauer(void)
 {
-  int h;
-  int m;
-  int d;
-
-  h = 0;
-  while (h < 24)
-    {
-      m = 0;
-      while ( m < 60)
-	{ 
-	  d = h / 10;
-	  _putchar('0' + d);
-	  d = h % 10;
-	  _putchar('0' + d);
-	  _putchar(':');
-	  d = m / 10;
-	  _putchar('0' + d);
-	  d = m % 10;
-	  _putchar('0' + d);
-	  _putchar('\n');
-	  m++;
-	  }
-      h++;
-	}
+int h;
+int m;
+int d;
+h = 0;
+while (h < 24)
+{
+m = 0;
+while (m < 60)
+{
+d = h / 10;
+_putchar('0' + d);
+d = h % 10;
+_putchar('0' + d);
+_putchar(':');
+d = m / 10;
+_putchar('0' + d);
+d = m % 10;
+_putchar('0' + d);
+_putchar('\n');
+m++;
+}
+h++;
+}
 }
