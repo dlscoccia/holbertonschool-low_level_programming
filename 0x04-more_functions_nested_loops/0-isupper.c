fc5a647fa8b567fcb1a0a52155091c851b33fd65
dlscoccia <2054@gmail.com>
2020-06-18 17:41:50 +0000

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
-
 /*
  * check for uppercase
  */int _isupper(int c)
- {
-	 if (c >= 'A' && c <= 'A')
-		 return (1);
-	 else
-		 return (0);
- }
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
