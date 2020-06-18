cd6748105e0cb4811b339f6eff8a389c14cc927b
dlscoccia <2054@gmail.com>
2020-06-18 19:16:47 +0000

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/*
+* print diagonal line n times
+*/ void print_diagonal(int n)
+{
+int a;
+int b;
+
+a = 0;
+b = 0;
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	while (a < n)
+	{
+		while (b <= a)
+		{
+			_putchar(' ');
+			b++;
+		}
+		b = 0;
+		a++;
+		_putchar(92);
+		_putchar('\n');
+	}
+}
+}
