180334d22f97dbb31d242bbe18f3de92fe1391a6
dlscoccia <2054@gmail.com>
2020-06-18 18:23:43 +0000

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/*
+* Fizz Buzz test
+*/ int main(void)
+{
+int a;
+
+a = 1;
+while (a <= 100)
+{
+	if ((a % 3 == 0) && (a % 5 == 0))
+	{
+		printf("FizzBuzz ");
+	}
+	else if (a % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if (a % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else
+	{
+		printf("%d ", a);
+	}
+	a++;
+}
+printf("\n");
+return (0);
+}
