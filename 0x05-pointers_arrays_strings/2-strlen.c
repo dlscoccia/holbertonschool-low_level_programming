d9304ed9652418de954ce794d6dea7426e86d4e7
dlscoccia <2054@gmail.com>
2020-06-24 16:55:35 +0000

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/*
+ * counts chars on a string
+ */ int _strlen(char *s)
+ {
+	 int a;
+	 int NULL = NULL;
+
+	 a = 0;
+	 while (*s != NULL)
+	 {
+		 a++;
+		 s++;
+	 }
+	 return(a);
+ }
