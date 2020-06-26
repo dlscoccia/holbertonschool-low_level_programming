cc5f04c64100ebd92d2a514422b4583861c0c497
dlscoccia <2054@gmail.com>
2020-06-26 19:05:00 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,23 @@
 #include "holberton.h"
 /*
-* prints a string in reverse
-*/void print_rev(char *s)
+ * prints a string in reverse
+ */void print_rev(char *s)
 {
-	char i = 0;
+char i = 0;
 
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-
-	while (i >= 0)
+while (*s != '\0')
+{
+	i++;
+	s++;
+}
+while (i >= 0)
+{
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		i--;
-		s--;
-		}
-
-	_putchar('\n');
+	}
+	i--;
+	s--;
+}
+_putchar('\n');
 }
