de5657bcdf2edcf1baf4f387df73d424a270ebe6
dlscoccia <2054@gmail.com>
2020-06-26 00:48:40 +0000

    h,1

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,2 +1,6 @@
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
