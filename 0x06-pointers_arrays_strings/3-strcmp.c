60040f1cd75bcb2330c8eef016807d77d6b094e0
dlscoccia <2054@gmail.com>
2020-06-26 02:04:07 +0000

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,21 +3,19 @@
  * compares strings
  */ int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
+	int i = 0;
+	int j = 0;
 
-while (s1[i] != '\0')
-{
-	i++;
-}
-while (s2[j] != '\0')
-{
-	j++;
-}
-if (i == j)
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		if (s1[i] != s2[j])
+		{
+
+			return (s1[i] - s2[j]);
+		}
+		i++;
+		j++;
+	}
 	return (0);
-if (i < j)
-	return (-15);
-else
-	return (15);
 }
