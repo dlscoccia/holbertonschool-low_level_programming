a28a893fa5e38e133b76787eb43f930157d25b66
dlscoccia <2054@gmail.com>
2020-06-26 01:08:31 +0000

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/*
+* funtion that copies a string
+*/ char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i <= n) /* && src[i] != '\0') */
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
