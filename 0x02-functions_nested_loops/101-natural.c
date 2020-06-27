9e14e5a06372b03901376f751df0024ddbf69cb3
dlscoccia <2054@gmail.com>
2020-06-27 15:18:50 +0000

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
