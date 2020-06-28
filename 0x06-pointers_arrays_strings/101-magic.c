5c0eb7739ede478f9412759cfb48444d51d035cd
dlscoccia <2054@gmail.com>
2020-06-28 01:07:14 +0000

    101

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -9,16 +9,7 @@
 
 	a[2] = 1024;
 	p = &n;
-	*(p + 5) = 98;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-/* ...so that this prints 98\n */
+	*(p + 5) = n = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
