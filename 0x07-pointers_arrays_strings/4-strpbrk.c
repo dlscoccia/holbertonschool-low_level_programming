0377c00e44c5b5e390df29630b76a37d34a2b023
dlscoccia <2054@gmail.com>
2020-06-30 02:44:56 +0000

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/*
+* search string for any set of bytes
+*/ char *_strpbrk(char *s, char *accept)
+{
+int i, j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+			return (&s[i]);
+	}
+}
+return (0);
+}
