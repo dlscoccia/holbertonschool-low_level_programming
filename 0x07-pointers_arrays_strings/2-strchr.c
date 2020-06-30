6ac3f963167b6c0fd61e54bf44000824d4bbd62b
dlscoccia <2054@gmail.com>
2020-06-30 00:27:26 +0000

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/*
+* function that locates a character in a string.
+*/ char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	i++;
+}
+return ('\0');
+}
