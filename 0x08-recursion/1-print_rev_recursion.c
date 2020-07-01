736efee887e2899b1cfca289e45a2ecae2578ea0
dlscoccia <2054@gmail.com>
2020-07-01 19:02:41 +0000

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/*
+ * Function - prints string in reverse with recursion
+ * @ entry point string
+ */ void _print_rev_recursion(char *s)
+ {
+	 if (*s == '\0')
+		 return;
+	 else
+	 {
+		 _print_rev_recursion(s + 1);
+		 _putchar(*s);
+	 }
+}
