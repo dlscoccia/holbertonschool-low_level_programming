e3351eccb52c99a65170c57f0c8b7ebfb1ef9fe2
dlscoccia <2054@gmail.com>
2020-07-01 19:37:22 +0000

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/*
+ * function that returns the value of x raised to the power of y
+ */ int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+		return (x * _pow_recursion(x, y + 1));
+	}
+	if (x == 1 || y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
