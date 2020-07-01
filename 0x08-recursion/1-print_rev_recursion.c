745d9792b109777709cc17f431577bb4c3504da7
dlscoccia <2054@gmail.com>
2020-07-01 19:24:30 +0000

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,12 +3,12 @@
  * Function - prints string in reverse with recursion
  * @ entry point string
  */ void _print_rev_recursion(char *s)
- {
-	 if (*s == '\0')
-		 return;
-	 else
-	 {
-		 _print_rev_recursion(s + 1);
-		 _putchar(*s);
-	 }
+{
+	if (*s == '\0')
+		return;
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
