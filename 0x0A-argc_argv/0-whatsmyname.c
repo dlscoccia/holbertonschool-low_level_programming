2faaf433998c487a15972387ddf94963cdf41c07
dlscoccia <2054@gmail.com>
2020-07-05 21:59:32 +0000

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/*
+* whats my name
+*/ int main(int argc, char **argv)
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
