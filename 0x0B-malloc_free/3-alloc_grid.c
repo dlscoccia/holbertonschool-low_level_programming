988a2678cfe585bc7f84b6837887c6d38dafaba6
dlscoccia <2054@gmail.com>
2020-07-08 00:48:29 +0000

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,30 +1,36 @@
 #include "holberton.h"
 #include <stdlib.h>
 /*
- * prints a grid
- */ int **alloc_grid(int width, int height)
+* prints a grid
+*/ int **alloc_grid(int width, int height)
 {
-int i = 0;
+	int i, j = 0;
 int **grid;
-if (width <= 0)
-{
-	return ('\0');
-}
-if (height <= 0)
+if (width <= 0 || height <= 0)
 {
 	return ('\0');
 }
 
-grid = malloc(sizeof(int *) * width);
-
-for (; i < width; i++)
-{
-	grid[i] = malloc(sizeof(int) * height);
-}
-
+grid = malloc(sizeof(int *) * height);
 if (grid == '\0')
 {
 	return ('\0');
 }
+
+for (i = 0; i < height; i++)
+{
+	grid[i] = malloc(sizeof(int) * width);
+	if (grid[i] == '\0')
+	{
+		for (i = i - 1; i >= 0; i--)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+		return ('\0');
+	}
+}
+for (i = 0; j < width; j++)
+	grid[i][j] = 0;
 return (grid);
 }
