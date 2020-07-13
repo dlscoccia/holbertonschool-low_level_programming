992ed8b196e9a7dbd2be8b268f28c9c5bad39523
dlscoccia <2054@gmail.com>
2020-07-13 23:09:36 +0000

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,9 +8,9 @@ typedef struct dog
 	char *owner;
 } my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-/*void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-*/
+int _putchar(char c);
+void print_dog(struct dog *d);
+/*dog_t *new_dog(char *name, float age, char *owner); 
+  void free_dog(dog_t *d); */
 
 #endif
