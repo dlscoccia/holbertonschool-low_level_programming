4286a19cf49ab6261f5ad8221961e4ebc6619b04
dlscoccia <2054@gmail.com>
2020-07-13 22:18:20 +0000

    R, 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HOLB
+#define HOLB
+
+/**
+ * struct dog - Defines a new type dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owners name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
