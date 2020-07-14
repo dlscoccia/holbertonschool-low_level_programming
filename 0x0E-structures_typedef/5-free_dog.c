dece39b5bb5bee61632e7d8ec0fd176e8e19072c
dlscoccia <2054@gmail.com>
2020-07-14 00:57:52 +0000

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ * free_dog - free allocated memory of dog_t
+ * @d: pointer to dog_t struct
+ * Return: void
  **/
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-
 	free(d->name);
 	free(d->owner);
-	free (d);
+	free(d);
 }
