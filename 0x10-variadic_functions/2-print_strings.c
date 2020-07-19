2206bf3351a052a0e977f5acc34f3f6ce439ebc3
dlscoccia <2054@gmail.com>
2020-07-19 23:27:36 +0000

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,27 @@
-#include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 /**
- * print_strings - prints the strings passed as arguments
- * @separator: sign to separate the strings
- * @n: number of elements to print
- * Return: void
- **/
+ * print_strings - Prints strings passed to the function separed by separators
+ * @separator: A string that is used to separate strings
+ * @n: number of unnamed parameters
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
-
 {
-	va_list list; /* declare list and variables */
-	char *temp_str; /* temporal str to allow me compare to null */
-	unsigned int i = 0;
+	char *str;
+	unsigned int i;
+	va_list a_list;
 
-	va_start(list, n); /* initilize list */
-	for (; i < n; i++) /* loop to go through string n elements */
+	va_start(a_list, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
 	{
-		temp_str = va_arg(list, char *); /* storing temp str */
-		if (temp_str != '\0') /* comparing entry str to null condition*/
-			printf("%s", temp_str);
-		else
-			printf("(nil)"); /* if str is null */
-		if (i + 1 < n) /* condition to print separator or \n at the end */
+		str = va_arg(a_list, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
 			printf("%s", separator);
-		else
-			printf("\n");
 	}
-	va_end(list); /* end list declaration */
+	printf("\n");
 }
