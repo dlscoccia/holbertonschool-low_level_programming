a8958f084604f22529ffbbd58bd22c56f713deea
dlscoccia <2054@gmail.com>
2020-07-24 04:26:27 +0000

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,23 +6,19 @@
  * Return: size of the list
  **/
 size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+	for (i = 1; h->next != NULL; i++)
 	{
-		size_t len;
-
 		if (h->str == NULL)
-			len = 0;
-		while (h != NULL)
-		{
-			/*	printf("[%d]", h->len); */
-
-			if (h->str != NULL)
-				printf("[%d] %s\n", h->len, h->str);
-			else
-
-				printf("[0] (nil)\n");
-			h = h->next;
-			len++;
-		}
-
-		return (len);
+			printf("[0] %s\n", "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
+}
