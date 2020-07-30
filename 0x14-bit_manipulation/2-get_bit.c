adc98e62fbf39d34213915c1f44202afda461a56
dlscoccia <2054@holbertonschool.com>
2020-07-30 21:33:04 +0000

    Adding 2-get_bit.c with Betty v1.0

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * get_bit - gets the bit on index
+ * @index: index to search
+ * @n: number given
+ * Return: bit on that index
+ **/
+int get_bit(unsigned long int n, unsigned int index)
+{
+int nbit;
+
+/* Right shift num, n times and perform bitwise AND with 1 */
+nbit = (n >> index) & 1;
+return (nbit);
+}
