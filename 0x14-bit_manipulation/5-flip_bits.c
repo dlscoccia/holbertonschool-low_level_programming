83fed52d317adccd34dcf988e9e32d17779ece54
dlscoccia <2054@holbertonschool.com>
2020-07-30 23:05:32 +0000

    Adding 5-flip_bits.c with Betty

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * flip_bits - flips bits of a number
+ * @m: 2nd number
+ * @n: 1st number
+ * Return: number of bits to flip
+ **/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int result, count = 0;
+
+result = n ^ m;
+
+while (result > 0)
+{
+if ((result & 1) == 1)
+count++;
+
+result = result >> 1;
+}
+return (count);
+}
