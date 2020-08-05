9e5a2a99a6cf83074dbb0ca6e487be4a84afd511
dlscoccia <2054@holbertonschool.com>
2020-08-05 14:44:14 +0000

    fixing 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
-int n_bytes;
+int n_bytes, n_write;
 char *buffer = malloc(sizeof(char) * letters);
 
 if (filename == 0 || buffer == NULL)
@@ -22,7 +22,12 @@ n_bytes = read(fd, buffer, letters);
 if (n_bytes == -1)
 return (0);
 
+free(buffer);
 close(fd);
-write(STDOUT_FILENO, buffer, n_bytes);
+
+n_write = write(STDOUT_FILENO, buffer, n_bytes);
+if (n_write == -1)
+return (0);
+
 return (n_bytes);
 }
