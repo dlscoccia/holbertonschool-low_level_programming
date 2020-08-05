074560e0521b649906f9e9abc1d8d847c602da92
dlscoccia <2054@holbertonschool.com>
2020-08-05 13:57:31 +0000

    updating 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,35 @@
 #include "holberton.h"
-
+/**
+ * create_file - create a new file
+ * @filename: name of the new file
+ * @text_content: content of the file
+ * Return: n
+ **/
 int create_file(const char *filename, char *text_content)
 {
 
+int fd_open = 0, fd_write = 0, iterator = 0;
 
+if (filename == NULL)
+return (-1);
 
+fd_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00400);
+if (fd_open == -1)
+return (-1);
+
+if (text_content == NULL)
+text_content = "";
+else
+{
+while (text_content[iterator] != '\0')
+{ iterator++; }
+}
+
+fd_write = write(fd_open, text_content, iterator);
+if (fd_write == -1)
+return (-1);
+
+close(fd_open);
+
+return (1);
 }
