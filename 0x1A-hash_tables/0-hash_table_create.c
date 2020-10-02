4ca88f2e9f3ff6ee5c70a3d4d3e234ad21ae2012
dlscoccia <2054@holbertonschool.com>
2020-10-02 19:33:23 +0000

    Fixing size == NULL return

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,20 +7,15 @@
  **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new_table = malloc(sizeof(hash_table_t) * 1);
+hash_table_t *new_table = malloc(sizeof(hash_table_t));
 unsigned int i = 0;
 
-if (new_table == NULL)
+if (size == 0 || new_table == NULL)
 return (NULL);
 
-new_table->array = malloc(sizeof(hash_node_t *) * size);
+new_table->array = calloc(size, sizeof(hash_node_t *));
 if (new_table->array == NULL)
 return (NULL);
-
-for (i = 0; i < size; i++)
-{
-new_table->array[i] = NULL;
-}
 
 return (new_table);
 }
