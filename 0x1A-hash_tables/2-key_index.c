e2fa86f74886bd4730686a74d15df12a59b5433e
dlscoccia <2054@holbertonschool.com>
2020-10-02 23:06:20 +0000

    Adding task 2 v2; betty fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 /**
- *
- * @
+ * key_index - finds the index for a given key
+ * @size: of the array
+ * @key: key value
  * Return:
  **/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
