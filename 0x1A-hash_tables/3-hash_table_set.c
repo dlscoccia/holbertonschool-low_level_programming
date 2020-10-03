54f6b358aec45dde1163fc3291509cd1ea173f15
dlscoccia <2054@holbertonschool.com>
2020-10-03 00:30:34 +0000

    Adding empty Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function that sets a hash table
+ * @key: the key
+ * @value: value associated with the key
+ * Return: 1 on success; 0 on failure
+ **/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+if (key == NULL || key == "")
+return (0);
+
+
+
+return (1);
+}
