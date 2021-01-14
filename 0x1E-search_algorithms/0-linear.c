b3c7ba2daf824f75a99e178dba0a07444fbbed2a
dlscoccia <2054@holbertonschool.com>
2021-01-13 20:22:38 -0500

    Betty recheck

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "search_algos.h"
+
 /**
- * linear_search - searches a value in an array of integers using Linear search
- * @array: of int elements
- * @size: of the array
- * @value: to search for
- * Return: index of value if founded; otherwise -1
+ *linear_search - searches a value in an array of integers using Linear search
+ *@array: of int elements
+ *@size: of the array
+ *@value: to search for
+ *Return: index of value if founded; otherwise -1
  **/
 
 int linear_search(int *array, size_t size, int value)
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef _SEARCH_ALGOS_
 #define _SEARCH_ALGOS_
 
+/* Standard Library */
+#include <stdio.h>
+
 /* Search Algorith Functions Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
