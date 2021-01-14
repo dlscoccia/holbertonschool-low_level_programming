4fca4aa204ad055d6367034e8203c8dbbcc96a13
dlscoccia <2054@holbertonschool.com>
2021-01-14 11:19:16 -0500

    adding size == 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,19 +1,18 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the
- * Linear search algorithm
- * @array: pointer to the first element of the array to search in
- * @size:  number of elements in array
- * @value: value to search for
- *
- * Return: First index where value is located or -1
- */
+ *linear_search - searches a value in an array of integers using Linear search
+ *@array: of int elements
+ *@size: of the array
+ *@value: to search for
+ *Return: index of value if founded; otherwise -1
+ **/
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
